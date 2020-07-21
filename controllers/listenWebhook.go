895b32655a98440954ad24d6f5ef1a407bbb2d7e
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-07-21 19:12:02 +0400

    Add test inline button

--- controllers/listenWebhook.go
+++ controllers/listenWebhook.go
@@ -3,22 +3,53 @@ package controllers
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/gramilul123/telegram-echo-bot/client"
 )
 
+// ListenWebhook listens calls from telegram api server
 func ListenWebhook(w http.ResponseWriter, r *http.Request) {
 	bytes, _ := ioutil.ReadAll(r.Body)
 	r.Body.Close()
 	var update tgbotapi.Update
 	json.Unmarshal(bytes, &update)
 
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-	msg.ReplyToMessageID = update.Message.MessageID
+	exampleQuery := "hello, world"
+	markup := tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.InlineKeyboardButton{
+				Text:         "Try it 11",
+				CallbackData: &exampleQuery,
+			},
+			tgbotapi.InlineKeyboardButton{
+				Text:         "Try it 12",
+				CallbackData: &exampleQuery,
+			},
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.InlineKeyboardButton{
+				Text:         "Try it 21",
+				CallbackData: &exampleQuery,
+			},
+			tgbotapi.InlineKeyboardButton{
+				Text:         "Try it 22",
+				CallbackData: &exampleQuery,
+			},
+		),
+	)
 
-	bot := client.TgBot{}
-	bot.Init()
-	bot.Client.Send(msg)
+	if update.Message != nil {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "hello, world!")
+		msg.ReplyMarkup = &markup
+
+		bot := client.TgBot{}
+		bot.Init()
+		bot.Client.Send(msg)
+	} else {
+		log.Println(update.CallbackQuery.Message.Text)
+	}
+
 }
