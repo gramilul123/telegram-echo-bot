8bf126d3f07e43d34e7bd43d00f4a9fd8d03342c
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-07-26 00:02:29 +0400

    Update Chat model

--- models/chat.go
+++ models/chat.go
@@ -1,7 +1,8 @@
 package models
 
 type Chat struct {
-	ID      int    `field:"id" key:"primary"`
+	ID      int    `field:"id" key:"primary" extra:"AUTO_INCREMENT"`
+	ChatID  int    `field:"chat_id"`
 	Command string `field:"command" len:"20"`
 	User    string `field:"user" len:"20"`
 	Time    int    `field:"time"`
