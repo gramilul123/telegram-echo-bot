ce22696d2d63bd95aff990c51782bc7514fa82b9
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-07-27 20:35:00 +0400

    Update dialog with bot

--- actions/actions.go
+++ actions/actions.go
@@ -1,1 +1,51 @@
 package actions
+
+import (
+	"fmt"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/gramilul123/telegram-echo-bot/models"
+)
+
+func StartBot(update tgbotapi.Update) (msg tgbotapi.MessageConfig) {
+
+	chat := models.Chat{
+		ChatID:  update.Message.Chat.ID,
+		Command: update.Message.Text,
+		Status:  models.OPEN,
+	}
+	models.Chats().DeleteByChatID(chat.ChatID)
+	models.Chats().Insert(chat)
+	msg = tgbotapi.NewMessage(chat.ChatID, "Hello!")
+
+	brows := [][]tgbotapi.KeyboardButton{}
+	brow := []tgbotapi.KeyboardButton{}
+	text := fmt.Sprintf("select_map")
+	brow = append(brow, tgbotapi.KeyboardButton{
+		Text: text,
+	})
+	brows = append(brows, brow)
+	markup := tgbotapi.NewReplyKeyboard(brows...)
+	msg.ReplyMarkup = &markup
+
+	return
+}
+
+func SelectMap(update tgbotapi.Update) (msg tgbotapi.MessageConfig) {
+	msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Your map")
+	brows := [][]tgbotapi.KeyboardButton{}
+	for i := 1; i <= 10; i++ {
+		brow := []tgbotapi.KeyboardButton{}
+		for j := 1; j <= 10; j++ {
+			text := fmt.Sprintf("⬜️ \n \n \n \n %d-%d", i, j)
+			brow = append(brow, tgbotapi.KeyboardButton{
+				Text: text,
+			})
+		}
+		brows = append(brows, brow)
+	}
+	markup := tgbotapi.NewReplyKeyboard(brows...)
+	msg.ReplyMarkup = &markup
+
+	return
+}
--- controllers/listenWebhook.go
+++ controllers/listenWebhook.go
@@ -2,46 +2,32 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/gramilul123/telegram-echo-bot/actions"
 	"github.com/gramilul123/telegram-echo-bot/client"
 )
 
 // ListenWebhook listens calls from telegram api server
 func ListenWebhook(w http.ResponseWriter, r *http.Request) {
+	var update tgbotapi.Update
+	var msg tgbotapi.MessageConfig
+
 	bytes, _ := ioutil.ReadAll(r.Body)
 	r.Body.Close()
-	var update tgbotapi.Update
+
 	json.Unmarshal(bytes, &update)
 
-	brows := [][]tgbotapi.KeyboardButton{}
-	for i := 1; i <= 10; i++ {
-		brow := []tgbotapi.KeyboardButton{}
-		for j := 1; j <= 10; j++ {
-			text := fmt.Sprintf("%v-%v", i, j)
-			brow = append(brow, tgbotapi.KeyboardButton{
-				Text: text,
-			})
-		}
-		brows = append(brows, brow)
-	}
-	markup := tgbotapi.NewReplyKeyboard(brows...)
+	log.Println(update.Message.Text)
+	if update.Message.Text == "/start" {
+		msg = actions.StartBot(update)
 
-	if update.Message != nil {
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "hello, world!")
-		msg.ReplyMarkup = &markup
-
-		bot := client.TgBot{}
-		bot.Init()
-		bot.Client.Send(msg)
-	} else {
-		msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.Text)
-		bot := client.TgBot{}
-		bot.Init()
-		bot.Client.Send(msg)
+	} else if update.Message.Text == "select_map" {
+		msg = actions.SelectMap(update)
 	}
 
+	client.Get().Client.Send(msg)
 }
