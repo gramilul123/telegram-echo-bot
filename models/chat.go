af34881e2c5a287039ba215a21c29f5bb35fb57a
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-07-27 17:10:41 +0400

    Update chat model

--- models/chat.go
+++ models/chat.go
@@ -1,9 +1,46 @@
 package models
 
+import (
+	"log"
+
+	"github.com/gramilul123/telegram-echo-bot/db"
+)
+
 type Chat struct {
-	ID      int    `field:"id" key:"primary" extra:"AUTO_INCREMENT"`
-	ChatID  int    `field:"chat_id"`
-	Command string `field:"command" len:"20"`
-	User    string `field:"user" len:"20"`
-	Time    int    `field:"time"`
+	ID      int    `db:"id" key:"primary" extra:"AUTO_INCREMENT"`
+	ChatID  int64  `db:"chat_id"`
+	Command string `db:"command" len:"20"`
+	User    string `db:"user" len:"20"`
+	Status  string `db:"status" len:"1"`
+	Time    int    `db:"time"`
+}
+
+func (chat *Chat) DeleteByChatID(chatId int64) {
+
+	deleteWhere := []string{}
+	deleteWhere = append(deleteWhere, "chat_id")
+
+	db.Delete(chat, deleteWhere)
+}
+
+// Insert func inserts row into Chat table
+func (object *Chat) Insert(chat Chat) {
+	db.Insert(chat)
+}
+
+func (chat *Chat) GetByChatId(chatId int64) {
+	chats := []Chat{}
+	selectWhere := make(map[string]interface{})
+	selectWhere["chat_id"] = chatId
+	err := db.GetDBConnect().DB.Select(&chats, db.GetSelectRequest(&Chat{}, selectWhere))
+	if err != nil {
+		log.Fatalf("Chat: GetByChatId: %s", err)
+	}
+	log.Println(chats)
+}
+
+func Chats() *Chat {
+	chat := &Chat{}
+
+	return chat
 }
