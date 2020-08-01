bf896181380c30b9334b9d7edc356b73724e6eac
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-08-02 00:42:48 +0400

    Update chat model

--- models/chat.go
+++ models/chat.go
@@ -12,53 +12,60 @@ const (
 )
 
 type Chat struct {
-	ID      int    `db:"id" key:"primary" extra:"AUTO_INCREMENT"`
-	ChatID  int64  `db:"chat_id"`
-	Command string `db:"command" len:"20"`
-	User    string `db:"user" len:"20"`
-	Status  string `db:"status" len:"1"`
-	Time    int    `db:"time"`
+	ID          int    `db:"id" key:"primary" extra:"AUTO_INCREMENT"`
+	ChatID      int64  `db:"chat_id"`
+	Status      string `db:"status" len:"1"`
+	MessageID   int    `db:"message_id"`
+	AcceptedMap string `db:"accepted_map" type:"text"`
 }
 
-func (chat Chat) DeleteByChatID(chatId int64) {
+// DeleteByChatID removes chat by chat id
+func (chat Chat) DeleteByChatID(chatID int64) {
+	deleteWhere := make(map[string]interface{})
 
-	deleteWhere := []string{}
-	deleteWhere = append(deleteWhere, "chat_id")
+	deleteWhere["chat_id"] = chatID
 
 	db.Delete(chat, deleteWhere)
 }
 
 // Insert func inserts row into Chat table
-func (Chat) Insert(chat Chat) {
-	db.Insert(chat)
+func (Chat) Insert(newChat Chat) {
+	db.Insert(newChat)
 }
 
-func (Chat) GetByChatId(chatId int64) Chat {
+// GetByChatID returns chat object by chat id
+func (Chat) GetByChatID(chatID int64) (chat Chat) {
 	chats := []Chat{}
 	selectWhere := make(map[string]interface{})
-	selectWhere["chat_id"] = chatId
+	selectWhere["chat_id"] = chatID
 
 	err := db.GetDBConnect().DB.Select(&chats, db.GetSelectRequest(&Chat{}, selectWhere))
 	if err != nil {
-		log.Fatalf("Chat: GetByChatId: %s", err)
+		log.Fatalf("Chat: GetByChatID: %s", err)
 	}
 
-	if len(chats) == 0 {
-		log.Fatal("Chat: GetByChatId: Chat %d not found", chatId)
+	if len(chats) > 0 {
+		chat.ID = chats[0].ID
+		chat.ChatID = chats[0].ChatID
+		chat.Status = chats[0].Status
+		chat.MessageID = chats[0].MessageID
+		chat.AcceptedMap = chats[0].AcceptedMap
 	}
-
-	return chats[0]
-}
-
-func (Chat) UpdateStatus(chat Chat, status string) Chat {
-	chat.Status = status
-	db.UpdateRow(chat, "chat_id")
 
 	return chat
 }
 
-func Chats(chatID ...int64) *Chat {
-	chat := &Chat{}
+// UpdateStatus updates status of chat
+func (Chat) UpdateStatus(chat Chat, status string) {
+	if chat.ChatID != 0 {
+		chat.Status = status
+		db.UpdateRow(chat, "chat_id")
+	}
+}
+
+// InstanceChat returns chat instans
+func InstanceChat() Chat {
+	chat := Chat{}
 
 	return chat
 }
