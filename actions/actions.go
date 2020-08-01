9b64ef147ec88cc6d4486c204b6845ce8f115e16
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-08-01 13:51:02 +0400

    Add reselect map function

--- actions/actions.go
+++ actions/actions.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"fmt"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	war_map "github.com/gramilul123/telegram-echo-bot/game/war_map"
 	"github.com/gramilul123/telegram-echo-bot/models"
@@ -29,7 +31,7 @@ func StartBot(update tgbotapi.Update) (msg tgbotapi.MessageConfig) {
 	return
 }
 
-func SelectMap(update tgbotapi.Update) (msg tgbotapi.MessageConfig) {
+func SelectMap(ChatID int64) (msg tgbotapi.MessageConfig) {
 	text := "Your map:\n"
 	gameMap := war_map.WarMap{}
 	gameMap.Create(true)
@@ -49,19 +51,79 @@ func SelectMap(update tgbotapi.Update) (msg tgbotapi.MessageConfig) {
 		}
 	}
 
-	msg = tgbotapi.NewMessage(update.Message.Chat.ID, text)
+	msg = tgbotapi.NewMessage(ChatID, text)
 
-	brows := [][]tgbotapi.KeyboardButton{}
-	brow := []tgbotapi.KeyboardButton{}
-	brow = append(brow, tgbotapi.KeyboardButton{
-		Text: "select_map",
+	brows := [][]tgbotapi.InlineKeyboardButton{}
+
+	brow := []tgbotapi.InlineKeyboardButton{}
+
+	callbackSelectMap := fmt.Sprintf("select_map")
+	textSelectMap := fmt.Sprintf("Select map")
+	brow = append(brow, tgbotapi.InlineKeyboardButton{
+		Text:         textSelectMap,
+		CallbackData: &callbackSelectMap,
 	})
-	brow = append(brow, tgbotapi.KeyboardButton{
-		Text: "accept",
+
+	callbackAccept := fmt.Sprintf("accept")
+	textAccept := fmt.Sprintf("Accept")
+	brow = append(brow, tgbotapi.InlineKeyboardButton{
+		Text:         textAccept,
+		CallbackData: &callbackAccept,
 	})
+
 	brows = append(brows, brow)
-	markup := tgbotapi.NewReplyKeyboard(brows...)
+
+	markup := tgbotapi.NewInlineKeyboardMarkup(brows...)
+
 	msg.ReplyMarkup = &markup
+
+	return
+}
+
+func ReSelectMap(ChatID int64, MessageID int) (editMsg tgbotapi.EditMessageTextConfig) {
+	text := "Your map:\n"
+	gameMap := war_map.WarMap{}
+	gameMap.Create(true)
+
+	for i, row := range gameMap.Cells {
+		if i > 0 && i < 11 {
+			for j, cell := range row {
+				if j > 0 && j < 11 {
+					if cell == war_map.Ship {
+						text += "⬛️"
+					} else {
+						text += "⬜️"
+					}
+				}
+			}
+			text += "\n"
+		}
+	}
+	editMsg = tgbotapi.NewEditMessageText(ChatID, MessageID, text)
+
+	brows := [][]tgbotapi.InlineKeyboardButton{}
+
+	brow := []tgbotapi.InlineKeyboardButton{}
+
+	callbackSelectMap := fmt.Sprintf("select_map")
+	textSelectMap := fmt.Sprintf("Select map")
+	brow = append(brow, tgbotapi.InlineKeyboardButton{
+		Text:         textSelectMap,
+		CallbackData: &callbackSelectMap,
+	})
+
+	callbackAccept := fmt.Sprintf("accept")
+	textAccept := fmt.Sprintf("Accept")
+	brow = append(brow, tgbotapi.InlineKeyboardButton{
+		Text:         textAccept,
+		CallbackData: &callbackAccept,
+	})
+
+	brows = append(brows, brow)
+
+	markup := tgbotapi.NewInlineKeyboardMarkup(brows...)
+
+	editMsg.ReplyMarkup = &markup
 
 	return
 }
