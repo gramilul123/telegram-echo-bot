2e155b0db9d81d64b12c06ea03b6d67c80cfafdc
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-08-02 23:29:25 +0400

    Replace methods

--- actions/actions.go
+++ actions/actions.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/gramilul123/telegram-echo-bot/db"
 	war_map "github.com/gramilul123/telegram-echo-bot/game/war_map"
 	"github.com/gramilul123/telegram-echo-bot/models"
 )
 
 // StartBot action after clicking Start button
 func StartBot(update tgbotapi.Update) (msg tgbotapi.MessageConfig, gameMap war_map.WarMap) {
-
 	chat := models.Chat{
 		ChatID: update.Message.Chat.ID,
 		Status: models.OPEN,
 	}
-	models.InstanceChat().DeleteByChatID(chat.ChatID)
-	models.InstanceChat().Insert(chat)
+
+	models.GetModel(models.CHAT).Delete("chat_id", chat.ID)
+	db.Insert(chat)
 
 	text, gameMap := getNewMapMsg()
 	markup := getSelectMapInlineMarkup()
@@ -38,11 +39,20 @@ func ReSelectMap(ChatID int64, MessageID int) (editMsg tgbotapi.EditMessageTextC
 
 // SaveMap save data to chat table
 func SaveMap(ChatID int64, MessageID int, gameMap war_map.WarMap) {
-	chat := models.InstanceChat().GetByChatID(ChatID)
+	chats := []models.Chat{}
+
+	models.GetModel(models.CHAT).Get("chat_id", ChatID, &chats)
+	chat := chats[0]
+
 	chat.MessageID = MessageID
 	chat.AcceptedMap = gameMap.MapToJson()
-	models.InstanceChat().UpdateChat(chat)
+
+	models.GetModel(models.CHAT).Update(chat, "chat_id", ChatID)
 }
+
+/*func Accept() {
+
+}*/
 
 // getMapMsg returns new map
 func getNewMapMsg() (text string, gameMap war_map.WarMap) {
