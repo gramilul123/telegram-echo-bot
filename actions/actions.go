3f34f44e7e02d5221e7b269bc2ea5dc7475506cc
gramilul123 <32932297+gramilul123@users.noreply.github.com>
2020-08-03 22:57:09 +0400

    Optimization actions list

--- actions/actions.go
+++ actions/actions.go
@@ -1,8 +1,6 @@
 package actions
 
 import (
-	"fmt"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/gramilul123/telegram-echo-bot/db"
 	"github.com/gramilul123/telegram-echo-bot/game/strategies"
@@ -45,10 +43,7 @@ func ReSelectMap(ChatID int64, MessageID int) (editMsg tgbotapi.EditMessageTextC
 
 // SaveMap save data to chat table
 func SaveMap(ChatID int64, MessageID int, gameMap war_map.WarMap) {
-	chats := []models.Chat{}
-
-	models.GetModel(models.CHAT).Get("chat_id", ChatID, &chats)
-	chat := chats[0]
+	chat := GetChat(ChatID)
 
 	chat.MessageID = MessageID
 	chat.AcceptedMap = gameMap.MapToJson()
@@ -56,46 +51,10 @@ func SaveMap(ChatID int64, MessageID int, gameMap war_map.WarMap) {
 	models.GetModel(models.CHAT).Update(chat, "chat_id", ChatID)
 }
 
-// CreateGame insert row to Game table
-func CreateGame(ChatID int64) {
-	chats := []models.Chat{}
-
-	models.GetModel(models.CHAT).Get("chat_id", ChatID, &chats)
-	chat := chats[0]
-
-	game := models.Game{
-		Status:    models.NewGame,
-		UserIDOne: ChatID,
-		WarMapOne: chat.AcceptedMap,
-	}
-
-	models.GetModel(models.GAME).Delete("user_id_one", ChatID)
-	db.Insert(game)
-}
-
 // Accept action after clicking Accept map
 func Accept(ChatID int64, MessageID int) (editMsg tgbotapi.EditMessageTextConfig) {
 	text := "Choose enemy"
-	brows := [][]tgbotapi.InlineKeyboardButton{}
-	brow := []tgbotapi.InlineKeyboardButton{}
-
-	callbackSelectMap := fmt.Sprintf(strategies.SIMPLE)
-	textSelectMap := fmt.Sprintf("Easy")
-	brow = append(brow, tgbotapi.InlineKeyboardButton{
-		Text:         textSelectMap,
-		CallbackData: &callbackSelectMap,
-	})
-
-	callbackAccept := fmt.Sprintf(strategies.MIDDLE)
-	textAccept := fmt.Sprintf("Middle")
-	brow = append(brow, tgbotapi.InlineKeyboardButton{
-		Text:         textAccept,
-		CallbackData: &callbackAccept,
-	})
-
-	brows = append(brows, brow)
-
-	markup := tgbotapi.NewInlineKeyboardMarkup(brows...)
+	markup := getEnemyVariants()
 	editMsg = tgbotapi.NewEditMessageText(ChatID, MessageID, text)
 	editMsg.ReplyMarkup = &markup
 
@@ -107,19 +66,7 @@ func Accept(ChatID int64, MessageID int) (editMsg tgbotapi.EditMessageTextConfig
 // Finish action
 func Finish(ChatID int64, MessageID int, result string) (editMsg tgbotapi.EditMessageTextConfig) {
 	text := result
-	brows := [][]tgbotapi.InlineKeyboardButton{}
-	brow := []tgbotapi.InlineKeyboardButton{}
-
-	callbackSelectMap := fmt.Sprintf("new")
-	textSelectMap := fmt.Sprintf("New game")
-	brow = append(brow, tgbotapi.InlineKeyboardButton{
-		Text:         textSelectMap,
-		CallbackData: &callbackSelectMap,
-	})
-
-	brows = append(brows, brow)
-
-	markup := tgbotapi.NewInlineKeyboardMarkup(brows...)
+	markup := getNewGameButton()
 	editMsg = tgbotapi.NewEditMessageText(ChatID, MessageID, text)
 	editMsg.ReplyMarkup = &markup
 
@@ -129,14 +76,11 @@ func Finish(ChatID int64, MessageID int, result string) (editMsg tgbotapi.EditMe
 // ChoseEnemy action after clicking enemy variant
 func ChoseEnemy(ChatID int64, MessageID int, enemy string) (editMsg tgbotapi.EditMessageTextConfig) {
 	var text string
-	games := []models.Game{}
 
-	models.GetModel(models.GAME).Get("user_id_one", ChatID, &games)
-	game := games[0]
+	game := GetGame(ChatID)
 
 	gameMapTwo := war_map.WarMap{}
 	gameMapTwo.Create(true)
-
 	game.UserIDTwo = EnemyToUser[enemy]
 	game.WarMapTwo = gameMapTwo.MapToJson()
 
@@ -145,85 +89,11 @@ func ChoseEnemy(ChatID int64, MessageID int, enemy string) (editMsg tgbotapi.Edi
 	gameMapOne := war_map.WarMap{}
 	gameMapOne.JsonToMap(game.WarMapOne)
 
-	for i, row := range gameMapOne.Cells {
-		if i > 0 && i < 11 {
-			for j, cell := range row {
-				if j > 0 && j < 11 {
-					if cell == war_map.Ship {
-						text += "⬛️"
-					} else {
-						text += "⬜️"
-					}
-				}
-			}
-			text += "\n"
-		}
-	}
+	text = getTextMap(text, gameMapOne.Cells)
 
-	brows := [][]tgbotapi.InlineKeyboardButton{}
-	brow := []tgbotapi.InlineKeyboardButton{}
-
-	callbackSelectMap := fmt.Sprintf("lose")
-	textSelectMap := fmt.Sprintf("Resign")
-	brow = append(brow, tgbotapi.InlineKeyboardButton{
-		Text:         textSelectMap,
-		CallbackData: &callbackSelectMap,
-	})
-
-	brows = append(brows, brow)
-
-	markup := tgbotapi.NewInlineKeyboardMarkup(brows...)
+	markup := getResignButton()
 	editMsg = tgbotapi.NewEditMessageText(ChatID, MessageID, text)
 	editMsg.ReplyMarkup = &markup
-
-	return
-}
-
-// getMapMsg returns new map
-func getNewMapMsg() (text string, gameMap war_map.WarMap) {
-	gameMap = war_map.WarMap{}
-	gameMap.Create(true)
-
-	for i, row := range gameMap.Cells {
-		if i > 0 && i < 11 {
-			for j, cell := range row {
-				if j > 0 && j < 11 {
-					if cell == war_map.Ship {
-						text += "⬛️"
-					} else {
-						text += "⬜️"
-					}
-				}
-			}
-			text += "\n"
-		}
-	}
-
-	return
-}
-
-// getSelectMapInlineMarkup returns inline buttons
-func getSelectMapInlineMarkup() (markup tgbotapi.InlineKeyboardMarkup) {
-	brows := [][]tgbotapi.InlineKeyboardButton{}
-	brow := []tgbotapi.InlineKeyboardButton{}
-
-	callbackSelectMap := fmt.Sprintf("select_map")
-	textSelectMap := fmt.Sprintf("Select map")
-	brow = append(brow, tgbotapi.InlineKeyboardButton{
-		Text:         textSelectMap,
-		CallbackData: &callbackSelectMap,
-	})
-
-	callbackAccept := fmt.Sprintf("accept")
-	textAccept := fmt.Sprintf("Accept")
-	brow = append(brow, tgbotapi.InlineKeyboardButton{
-		Text:         textAccept,
-		CallbackData: &callbackAccept,
-	})
-
-	brows = append(brows, brow)
-
-	markup = tgbotapi.NewInlineKeyboardMarkup(brows...)
 
 	return
 }
---
+++ actions/helpers.go
@@ -0,0 +1,130 @@
+package actions
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/gramilul123/telegram-echo-bot/db"
+	"github.com/gramilul123/telegram-echo-bot/game/strategies"
+	war_map "github.com/gramilul123/telegram-echo-bot/game/war_map"
+	"github.com/gramilul123/telegram-echo-bot/models"
+)
+
+// getMapMsg returns new map
+func getNewMapMsg() (text string, gameMap war_map.WarMap) {
+	gameMap = war_map.WarMap{}
+	gameMap.Create(true)
+
+	text = getTextMap(text, gameMap.Cells)
+
+	return
+}
+
+// getTextMap return text view of map
+func getTextMap(text string, warCells [][]int) string {
+
+	for i, row := range warCells {
+		if i > 0 && i < 11 {
+			for j, cell := range row {
+				if j > 0 && j < 11 {
+					if cell == war_map.Ship {
+						text += "⬛️"
+					} else {
+						text += "⬜️"
+					}
+				}
+			}
+			text += "\n"
+		}
+	}
+
+	return text
+}
+
+// getSelectMapInlineMarkup returns inline buttons
+func getSelectMapInlineMarkup() (markup tgbotapi.InlineKeyboardMarkup) {
+	buttonList := make(map[string]string)
+	buttonList["select_map"] = "Choose map"
+	buttonList["accept"] = "Accept"
+	markup = getInlineButtons(buttonList)
+
+	return
+}
+
+// getEnemyVariants returns inline buttons
+func getEnemyVariants() (markup tgbotapi.InlineKeyboardMarkup) {
+	buttonList := make(map[string]string)
+	buttonList[strategies.SIMPLE] = "Easy"
+	buttonList[strategies.MIDDLE] = "Middle"
+	markup = getInlineButtons(buttonList)
+
+	return
+}
+
+// getNewGameButton return New game inline button
+func getNewGameButton() (markup tgbotapi.InlineKeyboardMarkup) {
+	buttonList := make(map[string]string)
+	buttonList["new"] = "New game"
+	markup = getInlineButtons(buttonList)
+
+	return
+}
+
+// getNewGameButton return Resign inline button
+func getResignButton() (markup tgbotapi.InlineKeyboardMarkup) {
+
+	buttonList := make(map[string]string)
+	buttonList["lose"] = "Resign"
+	markup = getInlineButtons(buttonList)
+
+	return
+}
+
+// getInlineButtons returns inline buttons
+func getInlineButtons(buttonList map[string]string) (markup tgbotapi.InlineKeyboardMarkup) {
+	brows := [][]tgbotapi.InlineKeyboardButton{}
+
+	brow := []tgbotapi.InlineKeyboardButton{}
+	for callback, text := range buttonList {
+		callbackSelectMap := callback
+		textSelectMap := text
+		brow = append(brow, tgbotapi.InlineKeyboardButton{
+			Text:         textSelectMap,
+			CallbackData: &callbackSelectMap,
+		})
+	}
+	brows = append(brows, brow)
+	markup = tgbotapi.NewInlineKeyboardMarkup(brows...)
+
+	return
+}
+
+// GetChat returns Chat by chat id
+func GetChat(ChatID int64) (chat models.Chat) {
+	chats := []models.Chat{}
+	models.GetModel(models.CHAT).Get("chat_id", ChatID, &chats)
+	chat = chats[0]
+
+	return chat
+}
+
+// GetGame returns Game by chat id
+func GetGame(ChatID int64) (game models.Game) {
+	games := []models.Game{}
+	models.GetModel(models.GAME).Get("user_id_one", ChatID, &games)
+	game = games[0]
+
+	return game
+}
+
+// CreateGame insert row to Game table
+func CreateGame(ChatID int64) {
+	chat := GetChat(ChatID)
+
+	game := models.Game{
+		Status:    models.NewGame,
+		UserIDOne: ChatID,
+		WarMapOne: chat.AcceptedMap,
+	}
+
+	models.GetModel(models.GAME).Delete("user_id_one", ChatID)
+	db.Insert(game)
+}
